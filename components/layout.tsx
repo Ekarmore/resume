61854518d0e1741c5632493f2975cfbffea56a31
EKar <Earthb0y@outlook.com>
2022-12-18 21:03:49 +0800

    page build

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,13 +1,6 @@
-// import { Inter } from '@next/font/google';
-import { useState } from "react";
-
 export default function RootLayout({ children }: {
   children: React.ReactNode;
 }) {
-  const [isShow, setIsShow] = useState(true)
-  const onChange = (e:boolean) => {
-    setIsShow(e)
-  }
   return (
     <>
       <main>{children}</main>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,68 @@
 import Image from 'next/image'
 import Head from 'next/head'
 import avatar from "../public/Avatar.webp";
-import { useState } from 'react';
 export default function resume() {
   return (
-    <div className='relative'>
+    <div className='p-10 grid grid-cols-8 gap-10 h-screen leading-7'>
       <Head>
         <title> Resume | ekar </title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         {/* <link rel="icon" type='image' href="../public/Avatar.webp" /> */}
       </Head>
-      <section className='mx-auto flex flex-col flex-wrap w-78 justify-center items-center md:top-10 md:left-24 md:fixed'>
+      <section className='col-span-2 p-5  flex flex-col flex-wrap items-center'>
         <Image alt='my avatar' width={140} className='scale-1.5 rounded-full' placeholder='blur' src={avatar} />
-        <p className='font-bold text-2xl'>依克买提</p>
-        <div >前端开发</div>
-        <div className='flex items-center'><div className='text-xs border-b border-b-dashed'>earthb0y@outlook.com</div></div>
+        <p className='font-bold text-2xl mt-2'>依克买提</p>
+        <span>前端开发</span>
+        <a href='mailto:earthb0y@outlook.com' className='text-xs border-b border-dotted'>earthb0y@outlook.com</a>
       </section>
-      <article className='mx-auto w-1/4 mt-24'>
+      <article className='col-span-5 p-5'>
         <section>
           <div className='mb-5'>
-          <h4 className='w-auto pl-2 mb-2 text-lg font-extrabold bg-black text-white'>关于我</h4>
-            <p >你好,我是依克买提,一名前端开发,我对于编程的喜好是用良好的用户界面以及交互体将有意思的想法实现</p>
+          <h4 className='w-20 pl-2 mb-2 text-base font-extrabold bg-black text-white'>关于我</h4>
+            <p className='p-5'>你好，我是依克买提，一名前端开发，我对于编程的喜好是用良好的用户界面以及优秀的交互体让有趣的想法成为现实。</p>
           </div>
           <div className='mb-5' >
-            <h4 className='w-auto pl-2 mb-2 text-lg font-extrabold bg-black text-white' >教育经历</h4>
-            <div >
-              <p className='font-extrabold' >2018-2022</p>
-              <span>广东工业大学</span><span className='ml-2'>网络工程</span>
+            <h4 className='w-24 pl-2 mb-2 text-base font-extrabold bg-black text-white' >教育经历</h4>
+            <div className='p-5'>
+              <p className='font-extrabold text-sm' >2018-2022</p>
+              <span className='font-sans font-bold'>广东工业大学</span><span className='ml-2 font-sans font-bold'>网络工程</span>
             </div>
           </div>
+          <section className='mb-5' >
+            <h4 className='w-20 pl-2 mb-2 text-base font-extrabold bg-black text-white' >技术栈</h4>
+            <div className='p-5'>
+              <div className='Stack'>
+              <span className='font-sans font-bold mr-3'>前端</span><span>JavaScript,TypeScript,Vue,Nuxt,Vite,React,Next,TailWindCSS,WindiCSS,UnoCSS</span>
+              </div>
+              <div className='Stack'>
+              <span className='font-sans font-bold mr-3'>后端</span><span>Node,mongoDB,MySQL</span>
+              </div>
+              <div className='Stack'>
+              <span className='font-sans font-bold mr-3'>运维</span><span>阿里云,Netlify</span>
+              </div>
+              <div className='Stack'>
+              <span className='font-sans font-bold mr-3'>设计</span><span>Figma</span>
+              </div>
+            </div>
+          </section>
           <div className='mb-5'>
-            <h1 className='w-auto pl-2 mb-2 text-lg font-extrabold bg-black text-white' >项目</h1>
-            <div >
-              <p className='font-extrabold' >Aggr聚合搜索引擎</p>
-              <p className='font-extrabold' >Ekar-Template</p>
+            <h1 className='w-14 pl-2 mb-2 text-base font-extrabold bg-black text-white' >项目</h1>
+            <div className='grid grid-cols-8 gap-10 p-5'>
+              <div className='col-span-3 flex-wrap'>
+              <h4 className='projectTitle mb-2 font-sans' >Aggr</h4>
+              <span className='text-sm text-gray-500'>一个聚合搜索引擎，可以让用户快速指定引擎并且进行跳转</span>
+              <span className='text-sm text-gray-500'>Vue3/TailWindCss/Unocss</span>
+              </div>
+              <div className='col-span-3 col-start-5'>
+              <h4 className='projectTitle mb-2 font-sans' >Ekar-Template</h4>
+              <span className='text-sm text-gray-500'>一个聚合搜索引擎，可以让用户快速指定引擎并且进行跳转</span>
+              <span className='text-sm text-gray-500'>Vue3/TailWindCss/Unocss</span>
+              </div>
+              <div className='col-span-3 '>
+              <h4 className='projectTitle mb-2 font-sans' >Ekar-Template</h4>
+              <span className='text-sm text-gray-500'>一个聚合搜索引擎，可以让用户快速指定引擎并且进行跳转</span>
+              <span className='text-sm text-gray-500'>Vue3/TailWindCss/Unocss</span>
+              </div>
             </div>
           </div>
           <div>
